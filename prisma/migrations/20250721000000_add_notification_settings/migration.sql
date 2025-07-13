61cf4f35ce65ad9d56c717c430bd265ccd7daea4
LaoShui <laoshuikaixue@qq.com>
2025-07-13 22:43:43 +0800

    refactor(prisma): 移除重复的字段

--- prisma/migrations/20250721000000_add_notification_settings/migration.sql
+++
@@ -1,3 +0,0 @@
--- AlterTable
-ALTER TABLE "NotificationSettings" ADD COLUMN "songVotedThreshold" INTEGER NOT NULL DEFAULT 1;
-ALTER TABLE "NotificationSettings" ADD COLUMN "refreshInterval" INTEGER NOT NULL DEFAULT 60; 
