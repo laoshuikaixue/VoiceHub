0ebb467c210e7914fbee5b094dbf27f1b34c2a54
HelloFHZ <152045732+fhzit@users.noreply.github.com>
2026-05-23 09:30:21 +0000

    feat: 支持通过环境变量控制遥测

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -36,6 +36,7 @@ const backendSentryDsnDefault =
   'https://2fca0c8a939c8909e02c082ec847e8e8@o4508946125619200.ingest.de.sentry.io/4511244961448016'
 const frontendSentryDsnDefault =
   'https://3c4fe5353816bcdce36e7cc28703c8fa@o4508946125619200.ingest.de.sentry.io/4511244934774864'
+const telemetryEnabled = process.env.NUXT_DISABLE_TELEMETRY !== 'True'
 
 // 构造绝对路径 Logo URL 用于 SEO 标签，如果没有 host，则回退为相对路径
 const host = process.env.NUXT_PUBLIC_HOST
@@ -94,7 +95,7 @@ export default defineNuxtConfig({
       environment: process.env.SENTRY_ENVIRONMENT || process.env.NODE_ENV || 'development',
       release: process.env.SENTRY_RELEASE || process.env.VERCEL_GIT_COMMIT_SHA || process.env.COMMIT_REF || '',
       tracesSampleRate: readNumberEnv(process.env.SENTRY_TRACES_SAMPLE_RATE, 1),
-      enabled: process.env.NODE_ENV === 'production'
+      enabled: telemetryEnabled
     },
     // 公共键（会暴露到客户端）
     public: {
@@ -135,7 +136,7 @@ export default defineNuxtConfig({
           process.env.NUXT_PUBLIC_SENTRY_REPLAYS_ON_ERROR_SAMPLE_RATE,
           1
         ),
-        enabled: process.env.NODE_ENV === 'production'
+        enabled: telemetryEnabled
       }
     }
   },
