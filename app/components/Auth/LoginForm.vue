6ebdcf8e51fadcad1b102ae6de80ebede37681a0
zoerift805 <524913047@qq.com>
2026-09-06 09:48:59 +0800

    feat(legal): 优化法律条款页面和登录表单，修复样式和逻辑问题

--- app/components/Auth/LoginForm.vue
+++ app/components/Auth/LoginForm.vue
@@ -52,7 +52,8 @@
             <path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2" />
             <circle cx="12" cy="7" r="4" />
           </svg>
-          <input\n            :disabled="showLoginTerms && !loginTermsAccepted"\n            id="username"
+          <input
+            id="username"
             v-model="username"
             :class="{ 'input-error': error }"
             :autocomplete="!isBindMode && !showCreateMode ? 'username webauthn' : 'username'"
